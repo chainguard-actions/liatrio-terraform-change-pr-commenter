e3caf5c5dd9ffed7335c587f7fe8e70e60149abc
actionchain <actionchain@chainguard.dev>
2026-06-22 09:53:14 +0000

    Update liatrio/terraform-change-pr-commenter@v1.14.2

---
+++ CHANGELOG.md
@@ -0,0 +1,188 @@
+## [1.14.1](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.14.0...v1.14.1) (2026-01-29)
+
+
+### Bug Fixes
+
+* quiet mode for multiple files ([#99](https://github.com/liatrio/terraform-change-pr-commenter/issues/99)) ([f872e48](https://github.com/liatrio/terraform-change-pr-commenter/commit/f872e48129a4d2af4e811902e75c923954dfa471))
+
+# [1.14.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.13.0...v1.14.0) (2025-07-01)
+
+
+### Features
+
+* **action:** add support for `pull_request_target` event ([#95](https://github.com/liatrio/terraform-change-pr-commenter/issues/95)) ([fdfdbae](https://github.com/liatrio/terraform-change-pr-commenter/commit/fdfdbae2c1dbf02ed6a6334a334b99d2ed28f531)), closes [#94](https://github.com/liatrio/terraform-change-pr-commenter/issues/94)
+
+# [1.13.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.12.0...v1.13.0) (2025-06-27)
+
+
+### Features
+
+* Truncate PR comments if too long ([#93](https://github.com/liatrio/terraform-change-pr-commenter/issues/93)) ([5587334](https://github.com/liatrio/terraform-change-pr-commenter/commit/55873345015c06f59316309a71ea9433104ed5f9)), closes [#91](https://github.com/liatrio/terraform-change-pr-commenter/issues/91)
+
+# [1.12.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.11.0...v1.12.0) (2025-06-27)
+
+
+### Features
+
+* Simplify comment-header logic and improve multi-file support ([#92](https://github.com/liatrio/terraform-change-pr-commenter/issues/92)) ([de714c9](https://github.com/liatrio/terraform-change-pr-commenter/commit/de714c9edf0859d9851946cd93864c3b789e626a))
+
+# [1.11.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.10.1...v1.11.0) (2025-06-16)
+
+
+### Features
+
+* allow workflow calls as triggers ([#89](https://github.com/liatrio/terraform-change-pr-commenter/issues/89)) ([bd5d6aa](https://github.com/liatrio/terraform-change-pr-commenter/commit/bd5d6aafb8c90a8cc0e3d78c32a0f33fcffa27de))
+
+## [1.10.1](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.10.0...v1.10.1) (2025-05-30)
+
+
+### Bug Fixes
+
+* handle comment if no resources ([#88](https://github.com/liatrio/terraform-change-pr-commenter/issues/88)) ([4666dab](https://github.com/liatrio/terraform-change-pr-commenter/commit/4666dabe39891932028ca1b53a2172590009bad5))
+
+# [1.10.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.9.0...v1.10.0) (2025-04-29)
+
+
+### Features
+
+* Add tags tracking, unchanged resources, and matrix support for jobs ([#87](https://github.com/liatrio/terraform-change-pr-commenter/issues/87)) ([b2f5356](https://github.com/liatrio/terraform-change-pr-commenter/commit/b2f535616c169467e523e6ff35ba684606ee2521))
+
+# [1.9.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.8.0...v1.9.0) (2025-04-24)
+
+
+### Features
+
+* Add GitHub Actions to CodeQL analysis matrix ([#86](https://github.com/liatrio/terraform-change-pr-commenter/issues/86)) ([d410b01](https://github.com/liatrio/terraform-change-pr-commenter/commit/d410b015e84293a722b73dd529393aeaf2b2cacf))
+
+# [1.8.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.7.1...v1.8.0) (2025-04-24)
+
+
+### Features
+
+* Add job link option, resolves [#84](https://github.com/liatrio/terraform-change-pr-commenter/issues/84) ([#85](https://github.com/liatrio/terraform-change-pr-commenter/issues/85)) ([0b450ba](https://github.com/liatrio/terraform-change-pr-commenter/commit/0b450ba53b357f81b54bf117c9dab15e2fd6cda8))
+
+## [1.7.1](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.7.0...v1.7.1) (2024-12-12)
+
+
+### Bug Fixes
+
+* `hide-previous-comment` now can handel matrix strategy runs when given a unique `comment-header` ([#78](https://github.com/liatrio/terraform-change-pr-commenter/issues/78)) ([95ca081](https://github.com/liatrio/terraform-change-pr-commenter/commit/95ca081372ca566e0c8577d24353842679b527ee))
+
+# [1.7.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.6.0...v1.7.0) (2024-09-03)
+
+
+### Features
+
+* adds an input for showing the full list of changed resources in the action output ([#68](https://github.com/liatrio/terraform-change-pr-commenter/issues/68)) ([815d669](https://github.com/liatrio/terraform-change-pr-commenter/commit/815d669c00d3bcb18303a846c49ef49cd2ab19db))
+
+# [1.6.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.5.0...v1.6.0) (2024-09-03)
+
+
+### Features
+
+* ability to hide previous comments & fix for summary issue ([#65](https://github.com/liatrio/terraform-change-pr-commenter/issues/65)) ([08ba12c](https://github.com/liatrio/terraform-change-pr-commenter/commit/08ba12cd559192754993f85f0e45de37f095e248))
+
+# [1.5.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.4.5...v1.5.0) (2024-05-08)
+
+
+### Features
+
+* additional feature additions ([#62](https://github.com/liatrio/terraform-change-pr-commenter/issues/62)) ([1465fa2](https://github.com/liatrio/terraform-change-pr-commenter/commit/1465fa28232ee9e3b51c5db1fa43f2c2f4b971e4))
+* multiple feature addition [#58](https://github.com/liatrio/terraform-change-pr-commenter/issues/58)  ([#60](https://github.com/liatrio/terraform-change-pr-commenter/issues/60)) ([01f0321](https://github.com/liatrio/terraform-change-pr-commenter/commit/01f0321246312425041b0ee8eaa5ba9404f6f1ae))
+
+
+### Reverts
+
+* Revert " feat: multiple feature addition #58  (#60)" (#63) ([ab68bb6](https://github.com/liatrio/terraform-change-pr-commenter/commit/ab68bb614b5eb42957331703bf7afa80e1188186)), closes [#58](https://github.com/liatrio/terraform-change-pr-commenter/issues/58) [#60](https://github.com/liatrio/terraform-change-pr-commenter/issues/60) [#63](https://github.com/liatrio/terraform-change-pr-commenter/issues/63)
+
+## [1.4.5](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.4.4...v1.4.5) (2024-02-02)
+
+
+### Bug Fixes
+
+* Update actions to use actions with versions with node20 ([#56](https://github.com/liatrio/terraform-change-pr-commenter/issues/56)) ([2365114](https://github.com/liatrio/terraform-change-pr-commenter/commit/236511422a9364c7adb215a172eec7173ce18374))
+* Update release.yml ([#55](https://github.com/liatrio/terraform-change-pr-commenter/issues/55)) ([d527888](https://github.com/liatrio/terraform-change-pr-commenter/commit/d5278883cce02e2713a70a70b80adbf7390a7476))
+
+## [1.4.5](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.4.4...v1.4.5) (2024-02-02)
+
+
+### Bug Fixes
+
+* Update release.yml ([#55](https://github.com/liatrio/terraform-change-pr-commenter/issues/55)) ([d527888](https://github.com/liatrio/terraform-change-pr-commenter/commit/d5278883cce02e2713a70a70b80adbf7390a7476))
+
+## [1.4.4](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.4.3...v1.4.4) (2024-02-02)
+
+
+### Bug Fixes
+
+* update line 24 to node20 ([#53](https://github.com/liatrio/terraform-change-pr-commenter/issues/53)) ([d84a976](https://github.com/liatrio/terraform-change-pr-commenter/commit/d84a976ce8bee75080d34f46b39d1c28f2cfec3a))
+
+## [1.4.2](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.4.1...v1.4.2) (2024-02-01)
+
+
+### Bug Fixes
+
+* Node bump to 20 from 16 ([#51](https://github.com/liatrio/terraform-change-pr-commenter/issues/51)) ([2da4521](https://github.com/liatrio/terraform-change-pr-commenter/commit/2da45213eef1edc22a05b22bb198b0d94e0cf2a0))
+
+# [1.4.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.3.3...v1.4.0) (2023-01-09)
+
+
+### Features
+
+* add job summary ([#36](https://github.com/liatrio/terraform-change-pr-commenter/issues/36)) ([01d3b49](https://github.com/liatrio/terraform-change-pr-commenter/commit/01d3b49e93cf0319d28dee2b3fbab268b6566df9))
+
+## [1.3.2](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.3.1...v1.3.2) (2022-06-09)
+
+
+### Bug Fixes
+
+* [#30](https://github.com/liatrio/terraform-change-pr-commenter/issues/30) empty plan would failed the action ([#31](https://github.com/liatrio/terraform-change-pr-commenter/issues/31)) ([c4ea052](https://github.com/liatrio/terraform-change-pr-commenter/commit/c4ea0520ce5c086465dda4da3f75fca6527a60e0))
+
+## [1.3.1](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.3.0...v1.3.1) (2022-04-29)
+
+
+### Bug Fixes
+
+* operator for replace was broken in last fixes ([#25](https://github.com/liatrio/terraform-change-pr-commenter/issues/25)) ([e82bf94](https://github.com/liatrio/terraform-change-pr-commenter/commit/e82bf94e915ad440bd0c08e69dcd9ef748a74ed2))
+
+# [1.3.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.2.0...v1.3.0) (2022-04-29)
+
+
+### Features
+
+* added support for 'replace', and 'unchanged' ([#24](https://github.com/liatrio/terraform-change-pr-commenter/issues/24)) ([a3c45b0](https://github.com/liatrio/terraform-change-pr-commenter/commit/a3c45b0c735a3c4467729d0f94730c2af583a5e2))
+
+# [1.2.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.1.0...v1.2.0) (2022-04-01)
+
+
+### Features
+
+* adding comment block expand flag ([55d2573](https://github.com/liatrio/terraform-change-pr-commenter/commit/55d25736974196a554e7ed4d864224b54af0123d))
+
+# [1.1.0](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.0.2...v1.1.0) (2022-02-23)
+
+
+### Features
+
+* Support for multiple plans ([#13](https://github.com/liatrio/terraform-change-pr-commenter/issues/13)) ([9dc08b0](https://github.com/liatrio/terraform-change-pr-commenter/commit/9dc08b01a7f2000f9a8721c4be3479079642580e))
+
+## [1.0.2](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.0.1...v1.0.2) (2022-02-13)
+
+
+### Bug Fixes
+
+* using yarn build to remove npm dependency vulnerabilities ([3777d7b](https://github.com/liatrio/terraform-change-pr-commenter/commit/3777d7bb2204009d82f659632a95372e71c08dfc))
+
+## [1.0.1](https://github.com/liatrio/terraform-change-pr-commenter/compare/v1.0.0...v1.0.1) (2022-02-05)
+
+
+### Bug Fixes
+
+* adding branding to the action ([e40fd0c](https://github.com/liatrio/terraform-change-pr-commenter/commit/e40fd0c772ab36937de7e86ae71e3e6f013b5a70))
+
+# 1.0.0 (2022-01-29)
+
+
+### Features
+
+* initial release v1.0.0 ([90eaf6e](https://github.com/liatrio/terraform-change-pr-commenter/commit/90eaf6ef9875330479e2368eaf669099c740b006))
---
+++ index.js
@@ -0,0 +1,433 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+const expandDetailsComment = core.getBooleanInput("expand-comment");
+const includePlanSummary = core.getBooleanInput("include-plan-job-summary");
+const myToken = core.getInput("github-token");
+const octokit = github.getOctokit(myToken);
+const context = github.context;
+const inputFilenames = core.getMultilineInput("json-file");
+const commentHeader = core.getMultilineInput("comment-header");
+const commentFooter = core.getMultilineInput("comment-footer");
+const quietMode = core.getBooleanInput("quiet");
+const includeLinkToWorkflow = core.getBooleanInput("include-workflow-link");
+const includeLinkToJob = core.getBooleanInput("include-job-link");
+const hidePreviousComments = core.getBooleanInput("hide-previous-comments");
+const logChangedResources = core.getBooleanInput("log-changed-resources");
+const includeTagOnlyResources = core.getBooleanInput(
+  "include-tag-only-resources",
+);
+const includeUnchangedResources = core.getBooleanInput(
+  "include-unchanged-resources",
+);
+
+const MAX_COMMENT_LENGTH = 65536;
+
+// Get current job name from GitHub environment variable
+const currentJobName = process.env.GITHUB_JOB || "";
+const currentRunnerName = process.env.RUNNER_NAME || "";
+
+// Log the job name for debugging
+console.log("Current job name:", currentJobName);
+
+const workflowLink = includeLinkToWorkflow
+  ? `
+[Workflow: ${context.workflow}](${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId})
+`
+  : "";
+
+// Initialize job link as empty string
+let jobLink = "";
+
+// Function to get job ID from GitHub API
+async function getJobId() {
+  if (includeLinkToJob) {
+    try {
+      // Get all jobs for the current workflow run
+      const response = await octokit.rest.actions.listJobsForWorkflowRun({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: context.runId,
+      });
+
+      // Find the current job by name
+      const job = response.data.jobs.find(
+        (job) =>
+          job.runner_name === currentRunnerName &&
+          (job.name.endsWith(currentJobName) ||
+            job.name.startsWith(currentJobName)),
+      );
+
+      if (job) {
+        console.log(`Found job ID: ${job.id} for job name: ${job.name}`);
+        // Create job link with the numeric job ID
+        return `
+[Job: ${job.name}](${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}/job/${job.id})
+`;
+      } else {
+        console.log(`Could not find job with name: ${currentJobName}`);
+        console.log(`Jobs: \n${JSON.stringify(response.data.jobs, null, 2)}`);
+        return "";
+      }
+    } catch (error) {
+      console.error(`Error fetching job ID: ${error.message}`);
+      return "";
+    }
+  }
+  return "";
+}
+
+var allFilesHaveNoChanges = true;
+
+// GraphQL queries and mutations used for hiding previous comments
+const minimizeCommentQuery = /* GraphQL */ `
+  mutation minimizeComment($id: ID!) {
+    minimizeComment(input: { classifier: OUTDATED, subjectId: $id }) {
+      clientMutationId
+    }
+  }
+`;
+
+const commentsQuery = /* GraphQL */ `
+  query comments($owner: String!, $name: String!, $number: Int!) {
+    repository(owner: $owner, name: $name) {
+      pullRequest(number: $number) {
+        comments(last: 100, orderBy: { field: UPDATED_AT, direction: DESC }) {
+          nodes {
+            id
+            body
+            isMinimized
+          }
+        }
+      }
+    }
+  }
+`;
+
+const output = () => {
+  let body = "";
+  // for each file
+  for (const file of inputFilenames) {
+    const resource_changes =
+      JSON.parse(fs.readFileSync(file)).resource_changes || [];
+    try {
+      let changed_resources = resource_changes.filter((resource) => {
+        return resource.change.actions != ["no-op"];
+      });
+
+      if (logChangedResources) {
+        console.log("changed_resources", changed_resources);
+      }
+      if (Array.isArray(resource_changes) && resource_changes.length > 0) {
+        const resources_to_create = [],
+          resources_to_update = [],
+          resources_to_delete = [],
+          resources_to_replace = [],
+          resources_to_tag = [],
+          resources_unchanged = [];
+
+        // Deep comparison function to check if objects are identical after removing tags
+        const isTagOnlyChange = (before, after) => {
+          if (includeTagOnlyResources == false) {
+            return false;
+          }
+          const beforeCopy = JSON.parse(JSON.stringify(before || {}));
+          const afterCopy = JSON.parse(JSON.stringify(after || {}));
+
+          delete beforeCopy.tags;
+          delete beforeCopy.tags_all;
+          delete afterCopy.tags;
+          delete afterCopy.tags_all;
+
+          return JSON.stringify(beforeCopy) === JSON.stringify(afterCopy);
+        };
+
+        // for each resource changes
+        for (const resource of resource_changes) {
+          const change = resource.change;
+          const address = resource.address;
+
+          switch (change.actions[0]) {
+            default:
+              break;
+            case "no-op":
+              resources_unchanged.push(address);
+              break;
+            case "create":
+              resources_to_create.push(address);
+              break;
+            case "delete":
+              if (change.actions.length > 1) {
+                resources_to_replace.push(address);
+              } else {
+                resources_to_delete.push(address);
+              }
+              break;
+            case "update":
+              if (isTagOnlyChange(change.before, change.after)) {
+                resources_to_tag.push(address);
+              } else {
+                resources_to_update.push(address);
+              }
+              break;
+          }
+        }
+        // the body must be indented at the start otherwise
+        // there will be formatting error when comment is
+        // showed on GitHub
+        let planSummary = `<b>Terraform Plan: ${resources_to_create.length} to be created, ${resources_to_delete.length} to be deleted, ${resources_to_update.length} to be updated${includeTagOnlyResources ? `, ${resources_to_tag.length} to be tagged` : ""}, ${resources_to_replace.length} to be replaced, ${resources_unchanged.length} unchanged.</b>`;
+
+        let fullBody = `
+${commentHeader} for \`${file}\`
+<details ${expandDetailsComment ? "open" : ""}>
+<summary>
+${planSummary}
+</summary>
+${includeUnchangedResources ? details("unchanged", resources_unchanged, "•") : ""}
+${details("create", resources_to_create, "+")}
+${details("delete", resources_to_delete, "-")}
+${details("update", resources_to_update, "!")}
+${includeTagOnlyResources ? details("tag", resources_to_tag, "!") : ""}
+${details("replace", resources_to_replace, "+")}
+</details>
+${commentFooter.map((a) => (a == "" ? "\n" : a)).join("\n")}
+${workflowLink}
+${jobLink}
+`;
+
+        const hasChangesInFile =
+          resources_to_create.length > 0 ||
+          resources_to_delete.length > 0 ||
+          resources_to_update.length > 0 ||
+          resources_to_tag.length > 0 ||
+          resources_to_replace.length > 0;
+
+        if (hasChangesInFile) {
+          allFilesHaveNoChanges = false;
+        }
+
+        // In quiet mode, skip adding to body if this file has no changes
+        if (!quietMode || hasChangesInFile) {
+          if (fullBody.length > MAX_COMMENT_LENGTH) {
+            body += `
+${commentHeader} for \`${file}\`
+${planSummary}
+<p>Sorry, the detailed plan exceeded GitHub's comment size limit (${MAX_COMMENT_LENGTH} characters) and has been truncated. Please see the workflow run for the full plan output.</p>
+${commentFooter.map((a) => (a == "" ? "\n" : a)).join("\n")}
+${workflowLink}
+${jobLink}
+`;
+          } else {
+            body += fullBody;
+          }
+        }
+      } else {
+        // Only log this message, don't affect the allFilesHaveNoChanges flag
+        // since empty resource_changes means no changes for this specific file
+        console.log(
+          "No changes found in the plan for this file.",
+        );
+        // In quiet mode, don't add "no changes" message to body
+        if (!quietMode) {
+          body += `
+<p>There were no changes done to the infrastructure.</p>
+`;
+        }
+        core.info(
+          `"The content of ${file} did not result in a valid array or the array is empty... Skipping."`,
+        );
+      }
+    } catch (error) {
+      core.error(`${file} is not a valid JSON file. error: ${error}`);
+    }
+  }
+  return body;
+};
+
+const details = (action, resources, operator) => {
+  let str_title = "";
+  let str = "";
+
+  if (resources.length !== 0) {
+    if (action === "unchanged") {
+      str_title = "Unchanged resources";
+    } else {
+      str_title = `Resources to ${action}`;
+    }
+    str = `
+#### ${str_title}\n
+\`\`\`diff\n
+`;
+    for (const el of resources) {
+      // In the replace block, we show delete (-) and then create (+)
+      if (action === "replace") {
+        str += `- ${el}\n`;
+      }
+      str += `${operator} ${el}\n`;
+    }
+
+    str += "```\n";
+  }
+
+  return str;
+};
+
+const queryComments = (variables) => {
+  return octokit.graphql(commentsQuery, variables);
+};
+
+const minimizeComment = (variables) => {
+  return octokit.graphql(minimizeCommentQuery, variables);
+};
+
+const hideComments = () => {
+  core.info(`Hiding previous comments.`);
+
+  queryComments({
+    owner: context.repo.owner,
+    name: context.repo.repo,
+    number: context.issue.number,
+  })
+    .then((response) => {
+      core.info(
+        `Successfully retrieved comments for PR #${context.issue.number}.`,
+      );
+      const comments = response.repository.pullRequest.comments.nodes;
+
+      core.info(`Found ${comments.length} comments in the PR.`);
+
+      core.info(`Comment header used for matching is: ${commentHeader}`);
+
+      filteredComments = comments.filter(
+        (comment) =>
+          comment.body.includes("Terraform Plan:") ||
+          comment.body.includes(
+            "There were no changes done to the infrastructure.",
+          ),
+      );
+
+      core.info(
+        `Filtered down to ${filteredComments.length} comments created by this action.`,
+      );
+
+      filteredComments = filteredComments.filter((comment) =>
+        comment.body.includes(commentHeader),
+      );
+
+      core.info(
+        `Filtered down to ${filteredComments.length} comments created by this action.`,
+      );
+
+      filteredComments = filteredComments.filter((comment) =>
+        comment.body.includes(commentHeader),
+      );
+
+      core.info(
+        `Filtered down to ${filteredComments.length} comments that need to be minimized.`,
+      );
+
+      const minimizePromises = filteredComments
+        .filter((comment) => !comment.isMinimized)
+        .map((comment) => {
+          return minimizeComment({ id: comment.id }).catch((error) =>
+            core.error(
+              `Failed to minimize comment ${comment.id}: ${error.message}`,
+            ),
+          );
+        });
+
+      return Promise.all(minimizePromises)
+        .then(() => core.info("All minimize operations completed."))
+        .catch((error) =>
+          core.error(`Error during minimize operations: ${error.message}`),
+        );
+    })
+    .catch((error) =>
+      core.error(`Failed to retrieve comments: ${error.message}`),
+    );
+};
+
+// Main execution wrapped in an async function to allow for await
+async function run() {
+  try {
+    // Get job link if needed
+    if (includeLinkToJob) {
+      jobLink = await getJobId();
+      console.log("Job link generated:", jobLink);
+    }
+
+    let rawOutput = output();
+    let createComment = true;
+
+    console.log("hidePreviousComments", hidePreviousComments);
+    console.log(
+      "hidePreviousComments && context.eventName === pull_request",
+      hidePreviousComments && (context.eventName === "pull_request" || context.eventName === "pull_request_target"),
+    );
+    if (hidePreviousComments && (context.eventName === "pull_request" || context.eventName === "pull_request_target")) {
+      hideComments();
+    }
+
+    console.log("includePlanSummary", includePlanSummary);
+    if (includePlanSummary) {
+      core.info("Adding plan output to job summary");
+      core.summary
+        .addHeading("Terraform Plan Results")
+        .addRaw(rawOutput)
+        .write();
+    }
+
+    console.log("quietMode", quietMode);
+    console.log("allFilesHaveNoChanges", allFilesHaveNoChanges);
+    console.log("quietMode && allFilesHaveNoChanges", quietMode && allFilesHaveNoChanges);
+    if (quietMode && allFilesHaveNoChanges) {
+      core.info(
+        "quiet mode is enabled and there are no changes to the infrastructure.",
+      );
+      core.info("Skipping comment creation.");
+      createComment = false;
+    }
+
+    if (
+      context.eventName === "pull_request" ||
+      context.eventName === "pull_request_target" ||
+      context.eventName === "workflow_call"
+    ) {
+      // Verify we have PR context available in the case that it's a workflow_call event- should always pass for pull_request
+      if (context.issue && context.issue.number) {
+        core.info(
+          `Found PR # ${context.issue.number} from ${context.eventName} event - proceeding to comment.`,
+        );
+      } else {
+        core.info(
+          `${context.eventName} event detected but no PR context available.`,
+        );
+        core.info("Skipping comment creation.");
+        createComment = false;
+      }
+    } else {
+      core.info("Action doesn't seem to be running in a PR workflow context.");
+      core.info("Skipping comment creation.");
+      createComment = false;
+    }
+
+    if (createComment) {
+      core.info("Adding comment to PR");
+      core.info(`Comment: ${rawOutput}`);
+      octokit.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: rawOutput,
+      });
+      core.info("Comment added successfully.");
+    }
+    core.setOutput("comment-body", rawOutput);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+// Execute the main function
+run();
